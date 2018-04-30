031809c48a1c8b82e2ea35a1f394b78f6c84f373
Paul Capatina-Voila <paulkapa@yahoo.com>
2018-04-30 11:44:36 +0200

    First Commit

---
+++ src/main/java/com/nordicmotorhomes/NordicmotorhomesApplication.java
@@ -0,0 +1,12 @@
+package com.nordicmotorhomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordicmotorhomesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordicmotorhomesApplication.class, args);
+    }
+}
